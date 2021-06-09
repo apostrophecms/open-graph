d027080e7d75f70799e075fd14488e103072bed9
Stuart Romanek <stuart@apostrophecms.com>
2021-06-08 21:20:14 -0400

    ported, need to test

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const path = require('path');
 
 module.exports = {
   options: {
-    alias: 'ALIAS',
+    alias: 'openGraph',
   },
   bundle: {
     directory: 'modules',
     modules: getBundleModuleNames()
+  },
+  init(self) {
+    self.apos.template.append('head', '@apostrophecms/open-graph:tags');
+    if (!self.apos.baseUrl) {
+      self.apos.util.warnDevOnce(
+        'aposOgBaseUrl',
+        '⚠️ You do not have the `baseUrl` value set for this application. The Open Graph image will not work correctly without this set.'
+      );
+    }
+  },
+  components(self) {
+    return {
+      async tags(req, data) {},
+    };
   }
 };
 
---
+++ modules/@apostrophecms/open-graph-doc-type/index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  improve: '@apostrophecms/doc-type',
+  fields(self, options) {
+    if (options.openGraph !== false) {
+      return {
+        add: {
+          openGraphTitle: {
+            label: 'Open Graph Title',
+            type: 'string',
+            help: 'The title of your content without any branding such as your site name.'
+          },
+          openGraphDescription: {
+            label: 'Open Graph Description',
+            type: 'string',
+            textarea: true,
+            help: 'A brief description of the content, usually between 2 and 4 sentences. This will displayed below the title of the post.'
+          },
+          openGraphType: {
+            label: 'Open Graph Type',
+            type: 'string',
+            htmlHelp: 'The type of media of your content. See <a href="https://ogp.me/#types" target="_blank">https://ogp.me/#types</a>.'
+          },
+          // TODO this needs minSize and aspectRatio options when they exist
+          _openGraphImage: {
+            type: 'relationship',
+            label: 'Open Graph Image',
+            max: 1,
+            withType: '@apostrophecms/image'
+          }
+        },
+        group: {
+          seo: {
+            label: 'Open Graph',
+            fields: [
+              'openGraphTitle',
+              'openGraphDescription',
+              'openGraphType',
+              'openGraphImage'
+            ]
+          }
+        }
+      };
+    }
+  }
+};
